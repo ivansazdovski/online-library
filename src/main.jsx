ec44697178a3184f6c2fcf5f4ae94766698a4a83
ivansazdovski <ivan.sazdovski@hotmail.com>
2024-06-15 21:52:09 +0200

    add Homepage

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import Homepage from './pages/Homepage'
+import HomePage from './pages/Homepage'
 import ErrorPage from './pages/ErrorPage'
 import Footer from './components/Footer'
 import './index.css'
 
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Homepage />,
+    element: <HomePage />,
     errorElement: <ErrorPage />
   },
 ])
