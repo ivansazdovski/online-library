4029b3cf68ef4a7e389949a675505ff52e56da8a
ivansazdovski <ivan.sazdovski@hotmail.com>
2024-06-15 21:54:47 +0200

    fix typo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import HomePage from './pages/Homepage'
+import HomePage from './pages/HomePage'
 import ErrorPage from './pages/ErrorPage'
 import Footer from './components/Footer'
 import './index.css'
